2581ff08986c4a6383dc0bbcd2a2b63029d34283
Amit Singh <amit.singh5@bp.com>
2026-08-19 13:33:22 +0530

    Add HTTP request middleware

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
 ﻿using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+    await next();
+    Console.WriteLine($"Response: {context.Response.StatusCode}");
+}
+);
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"M2 - Request: {context.Request.Method} {context.Request.Path}");
+    await next();
+    Console.WriteLine($"M2 - Response: {context.Response.StatusCode}");
+}
+);
 app.Run();
